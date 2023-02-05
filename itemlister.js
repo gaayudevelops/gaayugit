9a36b243511f175e2a1dbb5fecd119a025beea69
gaayudevelops <124303954+gaayudevelops@users.noreply.github.com>
2023-02-05 22:54:41 +0530

    Add files via upload

--- itemlister.js
+++ itemlister.js
@@ -126,6 +126,7 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var newDes=" "+document.getElementById('description').value; 
 
   // Create new li element
   var li = document.createElement('li');
@@ -133,64 +134,33 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(newDes));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
-
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
-
-  //CREATE AN EDIT BUTTON
+@@ -42,7 +44,7 @@ function addItem(e){
   var edtBtn = document.createElement('button');
 
   // Add classes to edit button
   edtBtn.className = 'btn btn-danger btn-sm float-right edit';
+  edtBtn.className = 'btn btn-sm float-right edit';
 
  // Append text node
 edtBtn.appendChild(document.createTextNode('Edit'));
-
- // Append button to li
- li.appendChild(edtBtn);
-
- // Append li to list
- itemList.appendChild(li);
-
-
-}
-
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
-    }
-  }
-}
-
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
+@@ -74,7 +76,8 @@ function filterItems(e){
   var items = itemList.getElementsByTagName('li');
   // Convert to an array
   Array.from(items).forEach(function(item){
     var itemName = item.firstChild.textContent;
+    var itemName = item.childNodes[0].textContent + item.childNodes[1].textContent ;
+    //console.log(itemName);
     if(itemName.toLowerCase().indexOf(text) != -1){
       item.style.display = 'block';
     } else {
       item.style.display = 'none';
     }
-  });
+  })
+}
+}
 }
 
