85d291d7adc83cd29b31d59db8423415d47185f5
gaayudevelops <124303954+gaayudevelops@users.noreply.github.com>
2023-02-05 22:32:12 +0530

    Add files via upload

--- itemlister.js
+++ itemlister.js
@@ -19,12 +19,12 @@ let items=document.getElementsByClassName('list-group-item');
 items[2].style.backgroundColor='green';
 for(let i=0;i<items.length;i++)
 {
+   
     items[i].style.fontWeight ='bold';
-}
 }*/
 
-
 let li=document.getElementsByTagName('li');
+/*let li=document.getElementsByTagName('li');
 console.log(li);
 li[2].style.backgroundColor='green';
 for(let i=0;i<li.length;i++)
@@ -32,3 +32,12 @@ for(let i=0;i<li.length;i++)
     li[i].style.fontWeight ='bold';
     li[i].style.backgroundColor='yellow';
 }
+}*/
+let seconditem=document.querySelector
+('.list-group-item:nth-child(2)');
+seconditem.style.backgroundColor='green';
+
+let thirditem=document.querySelector
+('.list-group-item:nth-child(3)');
+//console.log(thirditem);
+thirditem.style="display:none";
